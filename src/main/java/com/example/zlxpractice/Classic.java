def25961729e60766c44ef9b8074d9d03381ded2
kaka <zhanglixin0624@163.com>
2026-03-10 18:36:22 +0800

    fib

--- src/main/java/com/example/zlxpractice/Classic.java
+++ src/main/java/com/example/zlxpractice/Classic.java
@@ -196,12 +196,62 @@ public class Classic {
     // 输入：n = 2
     // 输出：1
     // 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+    public int fib1(int n) {
+        if (n == 0){
+            return 0;
+        }
+        else if (n == 1){
+            return 1;
+        }
+        else{
+            return fib1(n-1) + fib1(n-2);
+        }
+    }
+
+    public int fib2(int n) {
+        if (n <= 1){
+            return n;
+        }
+        // 节省空间&降低复杂度
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
+
 
     // 爬楼梯 属于动态规划？---LeetCode 70
+    // 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢
+    // 输入：n = 3
+    // 输出：3
+    // 解释：有三种方法可以爬到楼顶。
+    // 1. 1 阶 + 1 阶 + 1 阶
+    // 2. 1 阶 + 2 阶
+    // 3. 2 阶 + 1 阶
+    public int climbStairs(int n) {
+        if (n <= 2){
+            return n;
+        }
+        // 节省空间&降低复杂度
+        int a = 1;
+        int b = 2;
+        for (int i = 3; i <= n; i++){
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+        //climbStairs(n-1)+climbStairs(n-2);
+    }
 
     public static void main(String[] args) {
         Classic classic = new Classic();
         String s = "aaabcbcba";
         System.out.println(classic.longestPalindrome(s));
+        System.out.println(classic.fib1(8));
     }
 }
